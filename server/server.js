9235b8344c8e5c6a320756af7e9ac56709c0e93e
cupcakesprinkle3 <79061264+cupcakesprinkle3@users.noreply.github.com>
2021-08-18 08:22:57 -0700

    added initial jwt functionality

--- server/server.js
+++ server/server.js
@@ -3,28 +3,12 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 
-// const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require('./utils/auth');
 
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// create a new Apollo server and pass in our schema data
-async function startApolloServer(typeDefs, resolvers) {
-  const server = new ApolloServer({ typeDefs, resolvers });
-  await server.start();
-  server.applyMiddleware({ app });
-  return server;
-}
-const server = startApolloServer(typeDefs, resolvers);
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   // context: authMiddleware
-// });
-
-// integrate Apollo server with the Express applicaiton as middleware
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -33,14 +17,23 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+// create a new Apollo server and pass in our schema data
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware });
+  await server.start();
+  // integrate Apollo server with the Express applicaiton as middleware
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where we can go to test our GQL API
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
   });
-});
+}
+const server = startApolloServer(typeDefs, resolvers);
