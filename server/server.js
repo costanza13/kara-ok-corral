12390e4fa7560e989eedb044f8ff1e60e8d50985
mcc <costanza+git@gmail.com>
2021-08-18 13:00:17 -0700

    `app.get(*)` fallback has to come after the apollo server middleware is applied

--- server/server.js
+++ server/server.js
@@ -17,16 +17,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-})
-
 // create a new Apollo server and pass in our schema data
 async function startApolloServer() {
   const server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware });
   await server.start();
   // integrate Apollo server with the Express applicaiton as middleware
   server.applyMiddleware({ app });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  })
 
   db.once('open', () => {
     app.listen(PORT, () => {
