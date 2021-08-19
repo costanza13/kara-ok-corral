c856c29642f977ce8d1e8cef02933c2d04086567
cupcakesprinkle3 <79061264+cupcakesprinkle3@users.noreply.github.com>
2021-08-19 07:51:27 -0700

    added one client query

---
+++ client/src/utils/queries.js
@@ -0,0 +1,25 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_PLAYLISTS = gql`
+   query playlists($username: String) {
+     playlists(username: $username) {
+       _id
+       name
+       createdAt
+       username
+       members {
+            username
+        }
+       songs {
+         _id
+         createdAt
+         username
+         title
+         artist
+         lyricsUrl
+         videoUrl
+         performance
+       }
+     }
+   }
+ `;
