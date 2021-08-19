3713923be5b46cd0f47109f859b096a79ac050a2
crosgar <cecilia.rossi.garcia@gmail.com>
2021-08-18 19:15:44 -0700

    rename song and playlist components

--- client/src/components/Song/index.js
+++ client/src/components/Song/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 
-const SongForm = () => {
+const Song = () => {
    return (
       <div>
-         <form>
+         <div className="song-form">
+            <form>
             <div className="mb-3">
                <input type="text" className="form-control" id="songTitle" placeholder="song title"></input>
             </div>
@@ -19,8 +20,16 @@ const SongForm = () => {
             </div>
             <button type="submit" class="btn btn-primary">add song</button>
          </form>
+         </div>
+
+         <div className="song-display">
+            <h5>Tu Me Dejaste de Querer</h5>
+            <p>C. Tangana</p>
+            <a href="https://genius.com/C-tangana-la-hungara-and-nino-de-elche-tu-me-dejaste-de-querer-lyrics" target="_blank">lyrics</a>
+            <a href="https://www.youtube.com/watch?v=ET5DAb4Bri0" target="_blank">youtube</a>
+         </div>
       </div>
    );
-}
+};
 
-export default SongForm;
+export default Song;
