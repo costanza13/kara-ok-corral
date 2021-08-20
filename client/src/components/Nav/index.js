f7c2e8330155ad85f4a8123833d76979a664abd6
crosgar <cecilia.rossi.garcia@gmail.com>
2021-08-20 13:48:03 -0700

    changed color scheme

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -8,7 +8,7 @@ const Nav = () => {
     Auth.logout();
   };
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
+    <header className="mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
           <h1>Kara-OK-Corral</h1>
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -32,10 +32,10 @@ const Dashboard = () => {
   return (
     <Container fluid>
       <Row>
-        <Col xs={12} md={8}>
+        <Col>
           <h2>{user.username}</h2>
         </Col>
-        <Col xs={6} md={4}>
+        <Col>
           <p>Friends: {user.friendCount}</p>
           <FriendsList friends={user.friends} />
         </Col>
