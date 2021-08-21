3fe8fa8765d92850e3b0890206044219c46605c9
cupcakesprinkle3 <79061264+cupcakesprinkle3@users.noreply.github.com>
2021-08-21 16:29:53 -0700

    created Friends List component

--- client/src/components/FriendList/index.js
+++ client/src/components/FriendList/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
+// import React from 'react';
 
-const FriendList = () => {
+// const FriendList = () => {
+//   return (
+//     <div>
+//       your friends list
+//     </div>
+//   );
+// };
+
+// export default FriendList;
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const FriendList = ({ friendCount, username, friends }) => {
+  if (!friends || !friends.length) {
+    return <p className="bg-dark text-light p-3">{username}, make some friends!</p>;
+  }
+
   return (
     <div>
-      your friends list
+      {friends.map(friend => (
+        <button className="btn w-100 display-block mb-2" key={friend._id}>
+          <Link to={`/profile/${friend.username}`}>{friend.username}</Link>
+        </button>
+      ))}
     </div>
   );
 };
