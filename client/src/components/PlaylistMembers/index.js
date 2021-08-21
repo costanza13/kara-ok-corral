512c0792b311a1b19f24cb5c03c33ca8b15f3e6b
mcc <costanza+git@gmail.com>
2021-08-20 23:34:19 -0700

    use select input to add members to playlist

--- client/src/components/PlaylistMembers/index.js
+++ client/src/components/PlaylistMembers/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_ME } from '../../utils/queries';
+import EasyEdit from "react-easy-edit";
 
 const PlaylistMembers = ({ members, canEdit, updateMembers }) => {
   const { loading, data } = useQuery(QUERY_ME);
@@ -17,13 +18,11 @@ const PlaylistMembers = ({ members, canEdit, updateMembers }) => {
 
   const handleAddMember = (e) => {
     const newMember = e.target.value;
-
     const updatedMembers = [...members];
     if (newMember) {
       if (members.indexOf(newMember) === -1 && friends.indexOf(newMember) !== -1) {
         updatedMembers.push(newMember);
         updateMembers(updatedMembers);
-
       } else {
         alert('You can only add your friends!');
       }
@@ -53,12 +52,20 @@ const PlaylistMembers = ({ members, canEdit, updateMembers }) => {
     partyMembers = <ul>{memberButtons}</ul>
   }
 
+  // build the "add a memnber" list
+  const leftToAdd = friends.filter(friend => members.indexOf(friend) === -1);
+  console.log('left to add', leftToAdd);
+
   return (
     <>
-      {partyMembers}
+      {partyMembers}{' '}
       {
-        canEdit
-          ? <input name="friend-to-add" type="text" placeholder="add a friend" onBlur={handleAddMember} />
+        canEdit && leftToAdd.length
+          ? 
+          <select defaultValue='' onChange={handleAddMember} className="btn btn-outline-primary member-button">
+            <option value='' disabled={true}>add a friend</option>
+            {leftToAdd.map(friend => { return <option>{friend}</option> } )}
+          </select>
           : ''
       }
     </>
