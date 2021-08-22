025971753048a48aa42c81f8c03dfa7572f23791
cupcakesprinkle3 <79061264+cupcakesprinkle3@users.noreply.github.com>
2021-08-22 08:29:58 -0700

    added hero to home

---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import { useEffect, useRef } from 'react';
+import { init } from 'ityped'
+import { Jumbotron, Container } from 'reactstrap';
+
+function Hero() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 80,
+            strings: ["song", "playlist", "party"],
+        });
+
+    }, []);
+    return (
+        <div className="jumbo">
+        <Jumbotron>
+            <Container fluid>
+                <div className="jumbo-text">
+                <h2 className="display-3">Your karaoke <span className = "jumbo-span" ref={textRef}></span> roundup</h2>
+                </div>
+            </Container>
+        </Jumbotron>
+        </div>
+
+    );
+
+}
+
+export default Hero;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <div>
-      Home
+      <Hero />
     </div>
   )
 }
