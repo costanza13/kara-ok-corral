9316f4ce3d5298c2d1963a0a4254c5b10d42692e
mcc <costanza+git@gmail.com>
2021-08-23 20:00:21 -0700

    use friendCount from user object, not state

--- client/src/pages/PublicProfile.js
+++ client/src/pages/PublicProfile.js
@@ -10,72 +10,66 @@ import { ADD_FRIEND } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const PublicProfile = props => {
-    const { username: userParam } = useParams();
+  const { username: userParam } = useParams();
 
-    const [addFriend] = useMutation(ADD_FRIEND);
+  const [addFriend] = useMutation(ADD_FRIEND);
 
-    const [friendCount, setFriendCount] = useState(-1);
+  const { loading, data } = useQuery(QUERY_USER, {
+    variables: { username: userParam }
+  });
 
-    const { loading, data } = useQuery(QUERY_USER, {
-        variables: { username: userParam }
-    });
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  const user = data.user;
 
-    if (loading) {
-        return <div>Loading...</div>;
+  // if (
+  //     Auth.loggedIn() &&
+  //     Auth.getProfile().data.username === userParam
+  // ) {
+  //     return "Welcome to your public profile page!"
+  // }
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
     }
-    const user = data.user;
+  };
 
-    // if (
-    //     Auth.loggedIn() &&
-    //     Auth.getProfile().data.username === userParam
-    // ) {
-    //     return "Welcome to your public profile page!"
-    // }
+  return (
+    <div>
+      <div className="flex-row mb-3">
+        <h2 className="bg-dark text-secondary p-3 display-inline-block">
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile
+        </h2>
+        <h3>{user.username} has {user.friendCount} {user.friendCount === 1 ? 'friend' : 'friends'}</h3>
+        <ul>
+          {user.playlists.map((playlist) => {
 
-    const handleClick = async () => {
-        try {
-            await addFriend({
-                variables: { id: user._id }
-            });
-        } catch (e) {
-            console.error(e);
-        }
-    };
+            return (
 
-    if (friendCount === -1) {
-        setFriendCount(user.friendCount);
-      }
+              <li key={'li' + playlist._id}>
+                <Link
+                  key={playlist._id}
+                  to={`/playlist/${playlist._id}`}
+                >{playlist.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
+      </div>
 
-    return (
-        <div>
-            <div className="flex-row mb-3">
-                <h2 className="bg-dark text-secondary p-3 display-inline-block">
-                    Viewing {userParam ? `${user.username}'s` : 'your'} profile
-                </h2>
-                <h3>{user.username} has {friendCount} {friendCount === 1 ? 'friend' : 'friends'}</h3>
-                    <ul>
-                        {user.playlists.map((playlist) => {
-                            
-                            return (
-                                
-                                <li key={'li' + playlist._id}>
-                                    <Link
-                                        key={playlist._id}
-                                        to={`/playlist/${playlist._id}`}
-                                    >{playlist.name}</Link>
-                                </li>
-                            );
-                        })}
-                </ul>
-                {userParam && (
-                    <button className="btn ml-auto" onClick={handleClick}>
-                        Add Friend
-                    </button>
-                )}
-            </div>
-
-        </div>
-    );
+    </div>
+  );
 };
 
 export default PublicProfile;
