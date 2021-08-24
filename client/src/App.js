ac75b78977d35fc69939c498870f684a7716c2e1
mcc <costanza+git@gmail.com>
2021-08-23 18:36:13 -0700

    set up Performance page

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from "@apollo/client/link/context";
 import Home from './pages/Home';
+import PublicProfile from './pages/PublicProfile';
 import Dashboard from './pages/Dashboard';
-import Party from './pages/Party';
 import Playlist from './pages/Playlist';
+import Party from './pages/Party';
+import Performance from './pages/Performance';
 import Nav from './components/Nav';
 import Login from './components/LoginForm';
 import Signup from './components/SignupForm';
-import PublicProfile from './pages/PublicProfile';
 import Footer from './components/Footer';
 
 
@@ -42,11 +43,12 @@ function App() {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/dashboard" component={Dashboard} />
-            <Route path="/party/:playlistId" component={Party} />
-            <Route path="/playlist/:playlistId" component={Playlist} />
+            <Route exact path="/party/:playlistId" component={Party} />
+            <Route exact path="/playlist/:playlistId" component={Playlist} />
+            <Route exact path="/performance/:performanceId" component={Performance} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
-            <Route path="/profile/:username" component={PublicProfile} />
+            <Route exact path="/profile/:username" component={PublicProfile} />
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
           </Switch>
         
